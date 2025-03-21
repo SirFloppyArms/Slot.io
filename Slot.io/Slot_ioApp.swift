8308eee5ddee35d0405f73b45e9510c50ad1bb67
Nolan Law <nolan.law@yahoo.com>
2025-03-20 20:03:20 -0500

    Initial Commit

---
+++ Slot.io/Slot_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_ioApp.swift
+//  Slot.io
+//
+//  Created by Nolan Law on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct Slot_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Slot.ioTests/Slot_ioTests.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_ioTests.swift
+//  Slot.ioTests
+//
+//  Created by Nolan Law on 2025-03-20.
+//
+
+import Testing
+@testable import Slot_io
+
+struct Slot_ioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
